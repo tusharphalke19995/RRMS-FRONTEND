7f21753ff5cb03e3a87d5c13eedd560d47abe8f6
Tushar Phalke <tusharphalke@Tushars-MacBook-Air.local>
2025-03-14 09:30:16 +0530

    User and Role Creation Component Done

---
+++ src/app/modules/admin/pages/manage-user-role/manage-user-role-dialog/manage-user-role-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-user-role-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-user-role-dialog.component.html',
+  styleUrl: './manage-user-role-dialog.component.scss'
+})
+export class ManageUserRoleDialogComponent {
+
+}
---
+++ src/app/modules/admin/pages/manage-user-role/search-user-role/search-user-role.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-user-role',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-user-role.component.html',
+  styleUrl: './search-user-role.component.scss'
+})
+export class SearchUserRoleComponent {
+
+}
