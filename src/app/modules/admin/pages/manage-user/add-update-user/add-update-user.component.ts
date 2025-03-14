e75394788ce248a4366db3b31469fb702eb2c81b
Tushar Phalke <tusharphalke@Tushars-MacBook-Air.local>
2025-03-14 09:32:01 +0530

    User and Role Creation Component Done

---
+++ src/app/modules/admin/pages/manage-user/add-update-user/add-update-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-update-user.component.html',
+  styleUrl: './add-update-user.component.scss'
+})
+export class AddUpdateUserComponent {
+
+}
---
+++ src/app/modules/admin/pages/manage-user/search-userlist/search-userlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-userlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-userlist.component.html',
+  styleUrl: './search-userlist.component.scss'
+})
+export class SearchUserlistComponent {
+
+}
