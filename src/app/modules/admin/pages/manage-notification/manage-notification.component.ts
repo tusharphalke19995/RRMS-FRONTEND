aca0eb2071ffe7a6ce53e0c97232b8f8da1d6b2e
kharade-p <kharadep0196@gmail.com>
2025-04-18 08:55:52 +0530

    side menu

--- src/app/modules/admin/pages/manage-notification/manage-notification.component.ts
+++ src/app/modules/admin/pages/manage-notification/manage-notification.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from "@angular/core";
+import {
+  CommonModule,
+  CurrencyPipe,
+  NgClass,
+  NgFor,
+  NgIf,
+  NgTemplateOutlet,
+} from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatRippleModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSort, MatSortModule } from "@angular/material/sort";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { RouterLink } from "@angular/router";
+import { TranslocoModule } from "@ngneat/transloco";
 
 @Component({
-  selector: 'app-manage-notification',
+  selector: "app-manage-notification",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './manage-notification.component.html',
-  styleUrl: './manage-notification.component.scss'
+  imports: [
+    NgIf,
+    RouterLink,
+    MatSelectModule,
+    MatDatepickerModule,
+    TranslocoModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    NgFor,
+    NgTemplateOutlet,
+    NgClass,
+    MatRippleModule,
+    CurrencyPipe,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  templateUrl: "./manage-notification.component.html",
+  styleUrl: "./manage-notification.component.scss",
 })
 export class ManageNotificationComponent {
+  alert: { type: string; message: string };
+  isLoading: boolean = false;
 
+  @ViewChild("sort1") sort1: MatSort;
+  @ViewChild("paginator1") paginator1: MatPaginator;
+  dataSource: MatTableDataSource<any>;
+
+  columns: any[] = [
+    { labelen: "Notification", labelhi: "Notification", property: "ntContent" },
+    { labelen: "Date", labelhi: "Date", property: "createdOn" },
+    { labelen: "Read Status", labelhi: "Read Status", property: "isRead" },
+    { labelen: "Action", labelhi: "Action", property: "action" },
+  ];
+
+  displayedColumns: string[] = ["ntContent", "createdOn", "isRead", "action"];
 }
--- src/app/modules/admin/pages/manage-org-mapping/manage-org-mapping.component.ts
+++ src/app/modules/admin/pages/manage-org-mapping/manage-org-mapping.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe, NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-manage-org-mapping',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    NgIf,
+    RouterLink,
+    MatSelectModule,
+    MatDatepickerModule,
+    TranslocoModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    NgFor,
+    NgTemplateOutlet,
+    NgClass,
+    MatRippleModule,
+    CurrencyPipe,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+],
   templateUrl: './manage-org-mapping.component.html',
   styleUrl: './manage-org-mapping.component.scss'
 })
 export class ManageOrgMappingComponent {
-
+  alert: { type: string; message: string };
+  isLoading: boolean = false;
 }
