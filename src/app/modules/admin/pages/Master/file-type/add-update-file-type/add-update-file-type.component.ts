d828fc597111f77a717410812cd05078e22c4b66
kharade-p <kharadep0196@gmail.com>
2025-04-21 12:10:46 +0530

    Changes added

---
+++ src/app/modules/admin/pages/Master/file-type/add-update-file-type/add-update-file-type.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-update-file-type',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-update-file-type.component.html',
+  styleUrl: './add-update-file-type.component.scss'
+})
+export class AddUpdateFileTypeComponent {
+
+}
---
+++ src/app/modules/admin/pages/Master/file-type/file-type.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-file-type',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './file-type.component.html',
+  styleUrl: './file-type.component.scss'
+})
+export class FileTypeComponent {
+
+}
