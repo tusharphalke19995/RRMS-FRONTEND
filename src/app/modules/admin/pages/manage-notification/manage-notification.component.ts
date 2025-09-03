fe2115abc33d90491f10ef1c9b0e1238008e3b8e
Tushar Phalke <tusharphalke@Tushars-MacBook-Air.local>
2025-09-03 23:41:41 +0530

    notification fix

--- src/app/modules/admin/pages/manage-notification/manage-notification.component.ts
+++ src/app/modules/admin/pages/manage-notification/manage-notification.component.ts
@@ -34,7 +34,7 @@ import { ConfirmationDialogComponent } from "./confirmation-dialog/confirmation-
 import { DashbaordService } from "../../dashbaord/dashboard.service";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatTabsModule } from "@angular/material/tabs";
-import { AuthService } from "/Users/tusharphalke/Documents/Freelancing_Work/RRMS-FRONTEND/src/app/core/auth/auth.service";
+import { AuthService } from "../../../../../../src/app/core/auth/auth.service";
 
 
 @Component({
